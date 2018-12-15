c32a3f42bffc4485bca5c71a792729cd5e1a0170
Nikita Maltsev <n.maltsev.dis@gmail.com>
2018-12-15 23:27:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Requirements
+
+### - gcc >= 7
+### - conan
+### - SDL2
+
+# Modules structure
+
+## -system 
+
+Contains base things, such as types, helper functions etc.
+
+## -graphics
+
+Contains entities for working with graphics.
+
+# How to build
+
+Run one of scripts from the `scripts` folder.
---
+++ modules/db/src/pch.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+
+//
+// STL
+//
+
+#include <algorithm>
+#include <functional>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <vector>
+
+
+//
+// fmt
+//
+
+#include <fmt/format.h>
+
+
+// extern
+
+#include <pqxx/pqxx>
+#include <timer/Timer.hpp>
+#include <logger/Logger.hpp>
